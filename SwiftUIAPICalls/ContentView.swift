2565830b3b0745a0ac34f8ccbd4bce5cbe3e7ca4
William Rozier <williamrozier@icloud.com>
2023-08-27 14:04:42 -0400

    API Calls

--- SwiftUIAPICalls/ContentView.swift
+++ SwiftUIAPICalls/ContentView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 
+struct URLImage: View {
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body: some View {
+        if let data = data, let uiimage = UImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
 
-
+        
+        }
+        else {
+        Image("video")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 130, height: 70)
+            .background(Color.gray)
+            .onAppear{
+                fetchData()
+                
+            }
+        
+    }
+}
+    private func fetchData(){
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url){
+            data, _, _, in
+            self.data = data
+            }
+        task.resume()
+        }
+    }
 
 struct ContentView: View {
     @StateObject   var  viewModel = ViewModel()
@@ -19,7 +56,7 @@ struct ContentView: View {
             List{
                 ForEach(viewModel.courses, id: \.self){ course in
                     HStack {
-                        Image("")
+                        URLImage(urlString: course.image)
                             .frame(width:130, height: 70)
                             .background(Color.gray)
                         
